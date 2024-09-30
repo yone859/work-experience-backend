92ecbe361cda27ab752e6a2d44558a5b7d684a23
YoneTom <yonetomo.asics@gmail.com>
2024-09-30 16:34:55 +0900

    corsエラー解消

--- src/router/routing.rs
+++ src/router/routing.rs
@@ -4,29 +4,25 @@ use http::{header::HeaderValue};
 use dotenv::dotenv;
 use crate::career_contents::{edit_career, show_career, create_career};
 use crate::auth::{sign_up, login};
-use tower_http::cors::CorsLayer;
+use tower_http::cors::{CorsLayer,Any};
 
 pub async  fn running_router()  {
-  let origins = [
-    "http://localhost:3001".parse::<HeaderValue>().unwrap(),
-];
+    let cors = CorsLayer::new()
+    .allow_origin(Any)  // 全てのオリジンを許可
+    .allow_methods(Any)  // 全てのHTTPメソッドを許可
+    .allow_headers(Any); // 全てのヘッダーを許可
 
     let app = axum::Router::new()
     
     //トップページ情報取得
     .route("/sign-up", axum::routing::post(sign_up::create_login_info))
-    .route("/login", axum::routing::post(login::check_password))
+    .route("/aaa", axum::routing::post(login::check_password))
     .route("/top", axum::routing::get(show_career::show_top))
     // .route("/other", axum::routing::get(handler_other));
     .route("/", axum::routing::get(handle_index))
     .route("/edit-work-experience", axum::routing::post(edit_career::updata_career))
     .route("/create-work-experience", axum::routing::post(create_career::insert_career))
-    .layer(
-      CorsLayer::new()
-          .allow_origin(origins)
-  );
-
-    
+    .layer(cors);
 
     dotenv().ok();
     axum::Server::bind(&env::var("HOST_NAME").unwrap().parse().unwrap())
