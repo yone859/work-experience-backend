9e11abb18861fc310d0c34e0652b9b53850af631
YoneTom <yonetomo.asics@gmail.com>
2024-10-03 13:17:27 +0900

    不要なルーティングコメントアウト

--- src/router/routing.rs
+++ src/router/routing.rs
@@ -1,5 +1,5 @@
 use std::env;
-use axum::response::{IntoResponse, Redirect};
+use axum::{response::{IntoResponse, Redirect},routing};
 use http::{header::HeaderValue};
 use dotenv::dotenv;
 use crate::career_contents::{edit_career, show_career, create_career};
@@ -15,13 +15,12 @@ pub async  fn running_router()  {
     let app = axum::Router::new()
     
     //トップページ情報取得
-    .route("/sign-up", axum::routing::post(sign_up::create_login_info))
-    .route("/aaa", axum::routing::post(login::check_password))
-    .route("/top", axum::routing::get(show_career::show_top))
-    // .route("/other", axum::routing::get(handler_other));
-    .route("/", axum::routing::get(handle_index))
-    .route("/edit-work-experience", axum::routing::post(edit_career::updata_career))
-    .route("/create-work-experience", axum::routing::post(create_career::insert_career))
+    // .route("/sign-up", routing::post(sign_up::create_login_info))
+    // .route("/login", routing::post(login::check_password))
+    .route("/top", routing::get(show_career::show_top))
+    // .route("/", routing::get(handle_index))
+    // .route("/edit-work-experience", routing::post(edit_career::updata_career))
+    // .route("/create-work-experience", routing::post(create_career::insert_career))
     .layer(cors);
 
     dotenv().ok();
@@ -30,22 +29,14 @@ pub async  fn running_router()  {
     .await
     .unwrap();
 
-    async fn handle_index()-> axum::response::Html<String> {
-      let tera = tera::Tera::new("templates/*").unwrap();
+    // async fn handle_index()-> axum::response::Html<String> {
+    //   let tera = tera::Tera::new("templates/*").unwrap();
     
-      let mut context = tera::Context::new();
-      context.insert("title", "Index page");
-      context.insert("message", "これはサンプルです。");
+    //   let mut context = tera::Context::new();
+    //   context.insert("title", "Index page");
+    //   context.insert("message", "これはサンプルです。");
     
-      let output = tera.render("index.html", &context);
-      axum::response::Html(output.unwrap())
-    }
-
-    async fn redirect_to_home() -> Redirect {
-      println!("Name is None");
-      Redirect::to("/top")
-    }
-      // async fn handler_other() -> String {
-      //   "This is other page...".to_string()
-      // }
+    //   let output = tera.render("index.html", &context);
+    //   axum::response::Html(output.unwrap())
+    // }
 }
