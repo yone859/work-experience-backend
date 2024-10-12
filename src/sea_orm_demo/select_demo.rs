b02cb8e80cb3248c8cc18b319fd157c6d4cad004
YoneTom <yonetomo.asics@gmail.com>
2024-10-12 17:25:09 +0900

    selectデモ修正

--- src/sea_orm_demo/select_demo.rs
+++ src/sea_orm_demo/select_demo.rs
@@ -8,28 +8,27 @@ use sea_orm::*;
 pub async  fn select_null_check()  {
     let db = DbInfo::new().await;
 
-    let from_staff_id:i32 = 1;
-    let by_staff_id:i32 = 2;
+    let start_id:i32 = 1;
+    let end_id:i32 = 2;
 
-    let result_select:Vec<entities::staffs::Model> = select_staff(from_staff_id, by_staff_id, &db.get_db_connection()).await.expect("database select error!");
+    let result_select:Vec<staffs::Model>
+        = select_staff(start_id, end_id, &db.get_db_con()).await.expect("database select error!");
 
-    for dev_tool_record in &result_select{
-        let mut sss:Vec<HashMap<String,String>> = Vec::new();
-        
-        let mut dev_tool_str:HashMap<String,String> = HashMap::new();
-        let mut dev_tool_project_no_str:HashMap<String,String> = HashMap::new();
-
-        if let Some(str) = dev_tool_record.remark.clone() {
-            if !str.is_empty() {dev_tool_str.insert(str , dev_tool_record.remark.as_deref().unwrap_or("").to_string());}
+    let mut staff_list:Vec<String> = Vec::new();
+    for staff in &result_select{        
+        if let Some(str) = staff.remark.clone() {
+            if !str.is_empty() {
+                staff_list.push(staff.staff_name.clone().unwrap());
+            }
         }
-
     }
 
+    println!("備考欄に記載があるスタッフ：{:?}", staff_list);
 }
 
 
-pub async fn select_staff(from: i32, by: i32, db: &DbConn) -> Result<Vec<entities::staffs::Model>, DbErr> {
-    let selected: Vec<entities::staffs::Model> = 
+pub async fn select_staff(from: i32, by: i32, db: &DbConn) -> Result<Vec<staffs::Model>, DbErr> {
+    let selected: Vec<staffs::Model> = 
         staffs::Entity::find()
         .having(staffs::Column::Id.between(from, by))
         .all(db)
